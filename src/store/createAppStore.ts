7035fb32aa6c0255e65e44f7bf1d5a704cdf2d45
Andrew Varnerin <andrew@varnerin.info>
2018-05-15 07:24:59 -0400

    note about silly redux stuff

--- src/store/createAppStore.ts
+++ src/store/createAppStore.ts
@@ -1,5 +1,8 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import { createRootReducer } from './createRootReducer';
+
+// redux enhances the existing react namespace in a way that breaks typings as of
+// 2018-05-15. using require avoids importing the type defs and breaking compilation
 // tslint:disable-next-line:no-var-requires
 const thunk = require('redux-thunk').default;
 
