aaa3078fb2cd9e6a5bf07226f84f0d8eca60a75a
Andrew Varnerin <andrew@varnerin.info>
2018-05-26 08:38:58 -0400

    make tests runnable again

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,32 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+class LocalStorageMock {
+  private store: any;
+  constructor() {
+    this.store = {};
+  }
+
+  public clear() {
+    this.store = {};
+  }
+
+  public getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  public setItem(key: string, value: any) {
+    this.store[key] = value.toString();
+  }
+
+  public removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+(global as any).localStorage = new LocalStorageMock;
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
